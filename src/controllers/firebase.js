3f33ac929e97b3ae8b16c4576ac4a1db4ac3cd86
Trevor-Koenig <trevorkoenig@att.net>
2023-11-30 13:31:17 -0700

    added firebase

---
+++ src/controllers/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDiRJ-3_uFC1SqwHRTJXnMULleCq5NxKGY",
+
+    authDomain: "portfolio-site-f4cd0.firebaseapp.com",
+
+    projectId: "portfolio-site-f4cd0",
+
+    storageBucket: "portfolio-site-f4cd0.appspot.com",
+
+    messagingSenderId: "647816770643",
+
+    appId: "1:647816770643:web:56e35a8b1452f52ad6c2a6"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
