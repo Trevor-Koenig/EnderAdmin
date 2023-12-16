198e34d8c95574de03ee345b54fe116acdfc0030
Trevor-Koenig <trevorkoenig4@gmail.com>
2023-12-15 16:50:57 -0800

    Added a theme

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,45 +1,86 @@
+import * as React from 'react';
 import { Outlet } from "react-router-dom";
+import { Button } from "@mui/material";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function Root() {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <>
-            <div id="sidebar">
-                <h1>React Router Contacts</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            id="q"
-                            aria-label="Search contacts"
-                            placeholder="Search"
-                            type="search"
-                            name="q"
-                        />
-                        <div
-                            id="search-spinner"
-                            aria-hidden
-                            hidden={true}
-                        />
-                        <div
-                            className="sr-only"
-                            aria-live="polite"
-                        ></div>
-                    </form>
-                    <form method="post">
-                        <button type="submit">New</button>
-                    </form>
-                </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contacts/1`}>Your Name</a>
-                        </li>
-                        <li>
-                            <a href={`/contacts/2`}>Your Friend</a>
-                        </li>
-                    </ul>
-                </nav>
+            <div>
+                Hello! Welcome!
             </div>
-            <div id="detail">
+            <Button variant="contained">Sign up</Button>
+            <Button variant="contained">Log in</Button>
+            <div>
+                <Box>
+                    <TextField
+                        label="With normal TextField"
+                        id="outlined-start-adornment"
+                        sx={{ m: 1, width: '25ch' }}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+                        }}
+                    />
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <OutlinedInput
+                            id="outlined-adornment-weight"
+                            endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+                            aria-describedby="outlined-weight-helper-text"
+                            inputProps={{
+                                'aria-label': 'weight',
+                            }}
+                        />
+                        <FormHelperText id="outlined-weight-helper-text">Weight</FormHelperText>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-amount"
+                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                            label="Amount"
+                        />
+                    </FormControl>
+                </Box>
+            </div>
+            <div>
                 <Outlet />
             </div>
         </>
--- src/theme.js
+++ src/theme.js
@@ -3,45 +3,83 @@ import { green, grey, red } from '@mui/material/colors';
 
 const rawTheme = createTheme({
     palette: {
+        mode: 'dark',
         primary: {
-            light: '#69696a',
-            main: '#28282a',
-            dark: '#1e1e1f',
+            main: '#4c91ff',
         },
         secondary: {
-            light: '#fff5f8',
-            main: '#ff3366',
-            dark: '#e62958',
-        },
-        warning: {
-            main: '#ffc071',
-            dark: '#ffb25e',
-        },
-        error: {
-            light: red[50],
-            main: red[500],
-            dark: red[700],
-        },
-        success: {
-            light: green[50],
-            main: green[500],
-            dark: green[700],
+            main: '#ffba4c',
         },
     },
-    typography: {
-        fontFamily: "'Work Sans', sans-serif",
-        fontSize: 14,
-        fontWeightLight: 300, // Work Sans
-        fontWeightRegular: 400, // Work Sans
-        fontWeightMedium: 700, // Roboto Condensed
+    shape: {
+        borderRadius: 10,
+    },
+    overrides: {
+        MuiAppBar: {
+            colorInherit: {
+                backgroundColor: '#689f38',
+                color: '#fff',
+            },
+        },
+    },
+    props: {
+        MuiAppBar: {
+            color: 'inherit',
+        },
+    },
+    components: {
+        MuiSwitch: {
+            styleOverrides: {
+                root: {
+                    width: 46,
+                    height: 27,
+                    padding: 0,
+                    margin: 8,
+                },
+                switchBase: {
+                    padding: 1,
+                    '&$checked, &$colorPrimary$checked, &$colorSecondary$checked': {
+                        transform: 'translateX(16px)',
+                        color: '#fff',
+                        '& + $track': {
+                            opacity: 1,
+                            border: 'none',
+                        },
+                    },
+                },
+                thumb: {
+                    width: 24,
+                    height: 24,
+                },
+                track: {
+                    borderRadius: 13,
+                    border: '1px solid #bdbdbd',
+                    backgroundColor: '#fafafa',
+                    opacity: 1,
+                    transition: 'background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+                },
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                    border: 0,
+                    borderRadius: 3,
+                    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+                    color: 'white',
+                    height: 48,
+                    padding: '0 30px',
+                },
+            },
+        },
     },
 });
 
 const fontHeader = {
     color: rawTheme.palette.text.primary,
     fontWeight: rawTheme.typography.fontWeightMedium,
-    fontFamily: "'Roboto Condensed', sans-serif",
-    textTransform: 'uppercase',
+    fontFamily: 'Montserrat',
 };
 
 const theme = {
@@ -50,7 +88,6 @@ const theme = {
         ...rawTheme.palette,
         background: {
             ...rawTheme.palette.background,
-            default: rawTheme.palette.common.white,
             placeholder: grey[200],
         },
     },
